abc4a5156a0ccf8f4b6042fd22747a0b62f2c2be
Meng Tian mac <tianmeng94@hotmail.com>
2016-02-06 23:44:10 +0000

    Remove responseInterpreter from Interpreter as the result of interpreting response envelope is different in wsdl and non-wsdl mode.

--- src/Interpreter.php
+++ src/Interpreter.php
@@ -4,44 +4,48 @@ namespace Meng\Soap;
 
 class Interpreter
 {
-    private $requestInterpreter;
-    private $responseInterpreter;
+    private $soap;
+    private $lastFunction;
+    private $lastArguments;
 
     public function __construct($wsdl, array $options = [])
     {
-        $this->requestInterpreter = new Soap($wsdl, $options);
-        $this->responseInterpreter = new Soap(null, array_merge($options, ['location'=>'', 'uri'=>'']));
+        $this->soap = new Soap($wsdl, $options);
     }
 
     /**
      * Interpret SOAP method and arguments to an request envelope.
      *
-     * @param $function_name
-     * @param $arguments
-     * @param null $options
-     * @param null $input_headers
+     * @param string $function_name
+     * @param array $arguments
+     * @param array $options
+     * @param array $input_headers
      * @return array
      */
     public function request($function_name, $arguments, $options = null, $input_headers = null)
     {
-        $this->requestInterpreter->feedRequest($function_name, $arguments, $options, $input_headers);
+        $this->soap->feedRequest($function_name, $arguments, $options, $input_headers);
+        $this->lastFunction = $function_name;
+        $this->lastArguments = $arguments;
         return [
-            'Endpoint' => $this->requestInterpreter->getEndpoint(),
-            'SoapAction' => $this->requestInterpreter->getSoapAction(),
-            'Version' => $this->requestInterpreter->getVersion(),
-            'Envelope' => $this->requestInterpreter->getRequest()
+            'Endpoint' => $this->soap->getEndpoint(),
+            'SoapAction' => $this->soap->getSoapAction(),
+            'Version' => $this->soap->getVersion(),
+            'Envelope' => $this->soap->getRequest()
         ];
     }
 
     /**
      * Interpret a response envelope to PHP objects.
      *
-     * @param $response
+     * @param string $response
      * @return mixed
      */
     public function response($response)
     {
-        $this->responseInterpreter->feedResponse($response);
-        return $this->responseInterpreter->interpret();
+        $this->soap->feedResponse($response);
+        $response = $this->soap->__call($this->lastFunction, $this->lastArguments);
+        $this->soap->feedResponse(null);
+        return $response;
     }
 }
