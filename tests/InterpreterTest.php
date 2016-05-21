ab9b4442de3680e14079e0ef57225030aa8de9ff
Meng Tian mac <tianmeng94@hotmail.com>
2016-05-21 11:45:04 +0100

    Add more tests, including: input headers, type map, class map, exceptions.

--- tests/InterpreterTest.php
+++ tests/InterpreterTest.php
@@ -8,7 +8,7 @@ class InterpreterTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function requestWithWsdl()
+    public function requestWsdlArrayArguments()
     {
         $interpreter = new Interpreter('http://www.webservicex.net/CurrencyConvertor.asmx?WSDL');
         $request = $interpreter->request('ConversionRate', [['FromCurrency' => 'AFA', 'ToCurrency' => 'ALL']]);
@@ -19,13 +19,118 @@ class InterpreterTest extends PHPUnit_Framework_TestCase
         $this->assertContains('http://schemas.xmlsoap.org/soap/envelope/', $request->getSoapMessage());
         $this->assertContains('ConversionRate', $request->getSoapMessage());
         $this->assertContains('FromCurrency', $request->getSoapMessage());
+        $this->assertContains('AFA', $request->getSoapMessage());
         $this->assertContains('ToCurrency', $request->getSoapMessage());
+        $this->assertContains('ALL', $request->getSoapMessage());
     }
 
     /**
      * @test
      */
-    public function responseWithWsdl()
+    public function requestWsdlObjectArguments()
+    {
+        $interpreter = new Interpreter('http://www.webservicex.net/CurrencyConvertor.asmx?WSDL');
+        $rate = new ConversionRate;
+        $rate->FromCurrency = 'AFA';
+        $rate->ToCurrency = 'ALL';
+        $request = $interpreter->request('ConversionRate', [$rate]);
+        $this->assertEquals('http://www.webservicex.net/CurrencyConvertor.asmx', $request->getEndpoint());
+        $this->assertEquals('http://www.webserviceX.NET/ConversionRate', $request->getSoapAction());
+        $this->assertEquals('1', $request->getSoapVersion());
+        $this->assertNotEmpty($request->getSoapMessage());
+        $this->assertContains('http://schemas.xmlsoap.org/soap/envelope/', $request->getSoapMessage());
+        $this->assertContains('ConversionRate', $request->getSoapMessage());
+        $this->assertContains('FromCurrency', $request->getSoapMessage());
+        $this->assertContains('AFA', $request->getSoapMessage());
+        $this->assertContains('ToCurrency', $request->getSoapMessage());
+        $this->assertContains('ALL', $request->getSoapMessage());
+    }
+
+    /**
+     * @test
+     */
+    public function requestWsdlInputHeaders()
+    {
+        $interpreter = new Interpreter('http://www.webservicex.net/CurrencyConvertor.asmx?WSDL');
+        $request = $interpreter->request(
+            'ConversionRate',
+            [['FromCurrency' => 'AFA', 'ToCurrency' => 'ALL']],
+            null,
+            [new SoapHeader('www.namespace.com', 'test_header', 'header_data')]
+        );
+        $this->assertEquals('http://www.webservicex.net/CurrencyConvertor.asmx', $request->getEndpoint());
+        $this->assertEquals('http://www.webserviceX.NET/ConversionRate', $request->getSoapAction());
+        $this->assertEquals('1', $request->getSoapVersion());
+        $this->assertNotEmpty($request->getSoapMessage());
+        $this->assertContains('http://schemas.xmlsoap.org/soap/envelope/', $request->getSoapMessage());
+        $this->assertContains('www.namespace.com', $request->getSoapMessage());
+        $this->assertContains('test_header', $request->getSoapMessage());
+        $this->assertContains('header_data', $request->getSoapMessage());
+        $this->assertContains('ConversionRate', $request->getSoapMessage());
+        $this->assertContains('FromCurrency', $request->getSoapMessage());
+        $this->assertContains('AFA', $request->getSoapMessage());
+        $this->assertContains('ToCurrency', $request->getSoapMessage());
+        $this->assertContains('ALL', $request->getSoapMessage());
+    }
+
+    /**
+     * @test
+     */
+    public function requestTypeMapToXML()
+    {
+        $interpreter = new Interpreter(
+            'http://www.webservicex.net/CurrencyConvertor.asmx?WSDL',
+            [
+                'typemap' => [
+                    [
+                        'type_name' => 'ConversionRate',
+                        'type_ns' => 'http://www.webserviceX.NET/',
+                        'to_xml' => function() {
+                            return "<ConversionRate><FromCurrency>OLD</FromCurrency><ToCurrency>NEW</ToCurrency></ConversionRate>";
+                        }
+                    ]
+                ]
+            ]
+        );
+
+        $request = $interpreter->request('ConversionRate', [[]]);
+        $this->assertEquals('http://www.webservicex.net/CurrencyConvertor.asmx', $request->getEndpoint());
+        $this->assertEquals('http://www.webserviceX.NET/ConversionRate', $request->getSoapAction());
+        $this->assertEquals('1', $request->getSoapVersion());
+        $this->assertNotEmpty($request->getSoapMessage());
+        $this->assertContains('http://schemas.xmlsoap.org/soap/envelope/', $request->getSoapMessage());
+        $this->assertContains('ConversionRate', $request->getSoapMessage());
+        $this->assertContains('FromCurrency', $request->getSoapMessage());
+        $this->assertContains('OLD', $request->getSoapMessage());
+        $this->assertContains('ToCurrency', $request->getSoapMessage());
+        $this->assertContains('NEW', $request->getSoapMessage());
+    }
+
+    /**
+     * @test
+     */
+    public function responseWsdl()
+    {
+        $responseMessage = <<<EOD
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <ConversionRateResponse xmlns="http://www.webserviceX.NET/">
+      <ConversionRateResult>-1</ConversionRateResult>
+    </ConversionRateResponse>
+  </soap:Body>
+</soap:Envelope>
+EOD;
+        $interpreter = new Interpreter('http://www.webservicex.net/CurrencyConvertor.asmx?WSDL');
+        $responseMessage = $interpreter->response($responseMessage, 'ConversionRate');
+        $this->assertInstanceOf('\StdClass', $responseMessage);
+        $this->assertEquals(['ConversionRateResult' => '-1'], (array)$responseMessage);
+    }
+
+    /**
+     * @test
+     */
+    public function responseWsdlOutputHeaders()
     {
         $responseMessage = <<<EOD
 <?xml version="1.0" encoding="utf-8"?>
@@ -45,6 +150,7 @@ EOD;
         $interpreter = new Interpreter('http://www.webservicex.net/CurrencyConvertor.asmx?WSDL');
         $outputHeaders = [];
         $responseMessage = $interpreter->response($responseMessage, 'ConversionRate', $outputHeaders);
+        $this->assertInstanceOf('\StdClass', $responseMessage);
         $this->assertEquals(['ConversionRateResult' => '-1'], (array)$responseMessage);
         $this->assertNotEmpty($outputHeaders);
     }
@@ -52,7 +158,96 @@ EOD;
     /**
      * @test
      */
-    public function requestWithWsdlSoapV12()
+    public function responseWsdlClassMap()
+    {
+        $responseMessage = <<<EOD
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <ConversionRateResponse xmlns="http://www.webserviceX.NET/">
+      <ConversionRateResult>-1</ConversionRateResult>
+    </ConversionRateResponse>
+  </soap:Body>
+</soap:Envelope>
+EOD;
+        $interpreter = new Interpreter('http://www.webservicex.net/CurrencyConvertor.asmx?WSDL', ['classmap' => ['ConversionRateResponse' => '\ConversionRateResponse']]);
+        $responseMessage = $interpreter->response($responseMessage, 'ConversionRate');
+        $this->assertInstanceOf('\ConversionRateResponse', $responseMessage);
+        $this->assertEquals(['ConversionRateResult' => '-1'], (array)$responseMessage);
+    }
+
+    /**
+     * @test
+     */
+    public function responseTypeMapFromXML()
+    {
+        $responseMessage = <<<EOD
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <ConversionRateResponse xmlns="http://www.webserviceX.NET/">
+      <ConversionRateResult>-1</ConversionRateResult>
+    </ConversionRateResponse>
+  </soap:Body>
+</soap:Envelope>
+EOD;
+        $interpreter = new Interpreter(
+            'http://www.webservicex.net/CurrencyConvertor.asmx?WSDL',
+            [
+                'typemap' => [
+                    [
+                        'type_name' => 'ConversionRateResponse',
+                        'type_ns' => 'http://www.webserviceX.NET/',
+                        'from_xml' => function() {
+                            $rateResponse = new ConversionRateResponse;
+                            $rateResponse->MockedResult = 100;
+                            return $rateResponse;
+                        }
+                    ]
+                ]
+            ]
+        );
+
+        $responseMessage = $interpreter->response($responseMessage, 'ConversionRate');
+        $this->assertInstanceOf('\ConversionRateResponse', $responseMessage);
+        $this->assertEquals(['MockedResult' => 100], (array)$responseMessage);
+    }
+
+    /**
+     * @test
+     */
+    public function responseWsdlDisableExceptions()
+    {
+        $interpreter = new Interpreter(null, ['uri'=>'www.uri.com', 'location'=>'www.location.com', 'exceptions' => false]);
+        $responseMessage = <<<EOD
+<SOAP-ENV:Envelope
+  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+   <SOAP-ENV:Body>
+       <SOAP-ENV:Fault>
+           <faultcode>SOAP-ENV:Server</faultcode>
+           <faultstring>Server Error</faultstring>
+           <detail>
+               <e:myfaultdetails xmlns:e="Some-URI">
+                 <message>
+                   My application didn't work
+                 </message>
+                 <errorcode>
+                   1001
+                 </errorcode>
+               </e:myfaultdetails>
+           </detail>
+       </SOAP-ENV:Fault>
+   </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+EOD;
+        $result = $interpreter->response($responseMessage, 'AnyMethod');
+        $this->assertInstanceOf('\SoapFault', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function requestWsdlSoapV12()
     {
         $interpreter = new Interpreter('http://www.webservicex.net/airport.asmx?WSDL', ['soap_version' => SOAP_1_2]);
         $request = $interpreter->request('GetAirportInformationByCountry', [['country' => 'United Kingdom']]);
@@ -68,7 +263,7 @@ EOD;
     /**
      * @test
      */
-    public function responseWithWsdlSoapV12()
+    public function responseWsdlSoapV12()
     {
         $responseMessage = <<<EOD
 <?xml version="1.0" encoding="utf-8"?>
@@ -176,4 +371,16 @@ EOD;
         $request = $interpreter->request('AnyMethod');
         $this->assertTrue($request instanceof SoapRequest);
     }
+}
+
+/** Test support only */
+class  ConversionRate
+{
+
+}
+
+/** Test support only */
+class ConversionRateResponse
+{
+
 }
